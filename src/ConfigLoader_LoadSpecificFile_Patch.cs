13cf40c8ba35f5f8a9ab131f3780ba51a768d39a
NBKRedSpy <54865934+NBKRedSpy@users.noreply.github.com>
2026-03-20 09:23:35 -0400

    WIP

---
+++ src/ConfigLoader_LoadSpecificFile_Patch.cs
@@ -0,0 +1,19 @@
+﻿using HarmonyLib;
+using MGSC;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QM_DataExport
+{
+    [HarmonyPatch(typeof(ConfigLoader), nameof(ConfigLoader.LoadSpecificFile))]
+    internal class ConfigLoader_LoadSpecificFile_Patch
+    {
+        public void Prefix(string path)
+        {
+
+        }
+    }
+}
